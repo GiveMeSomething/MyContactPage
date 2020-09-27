f712b96db0f47a51bd99b8ce761fa629eb8e93d2
MinhHT <minhbeo135@gmail.com>
2020-09-28 01:19:34 +0700

    Updates

---
+++ precache-manifest.4d795e80c9220de7328e0b95e44f346f.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "a87ec31b862b0d4cb150a57c9c53dc92",
+    "url": "/MyContactPage/index.html"
+  },
+  {
+    "revision": "d2253a44f007c337a941",
+    "url": "/MyContactPage/static/css/2.11829350.chunk.css"
+  },
+  {
+    "revision": "159fe374e4c604a62743",
+    "url": "/MyContactPage/static/css/main.a53aa7bb.chunk.css"
+  },
+  {
+    "revision": "d2253a44f007c337a941",
+    "url": "/MyContactPage/static/js/2.5ce7891d.chunk.js"
+  },
+  {
+    "revision": "3453b8997016469371284a28c0e873e2",
+    "url": "/MyContactPage/static/js/2.5ce7891d.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "159fe374e4c604a62743",
+    "url": "/MyContactPage/static/js/main.b180ca0b.chunk.js"
+  },
+  {
+    "revision": "8f1fa3fe28cc507ee664",
+    "url": "/MyContactPage/static/js/runtime-main.23bc665f.js"
+  }
+]);
